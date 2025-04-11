68d1e9e6cec9c5a05d79ed7813e0a6858f3725c4
Leon <138937349+Loslosser@users.noreply.github.com>
2025-04-11 08:59:42 +0200

    Update __init__.py

--- src/rai_compos_pub/__init__.py
+++ src/rai_compos_pub/__init__.py
@@ -10,19 +10,12 @@ from rai_compos_pub.cpw import CPWBend
 from rai_compos_pub.cpw_simple import CPWStraight_Simple
 from rai_compos_pub.cpw_simple import CPWBend_Simple
 
-from rai_compos_pub.ppc_kid import PPCKID
-
 from rai_compos_pub.raitext import RAIText
 
 from rai_compos_pub.vernier import Ruler_marker
 from rai_compos_pub.vernier import Vernier_marker
 from rai_compos_pub.vernier import Vernier_marker_ebeam
 
-from rai_compos_pub.opt_align_mark import Opt_align_large_mark
-from rai_compos_pub.opt_align_mark import Opt_align_mark_minor
-from rai_compos_pub.opt_align_mark import Opt_align_major_assembly
-from rai_compos_pub.opt_align_mark import Opt_align_minor_assembly
-from rai_compos_pub.opt_align_mark import Opt_align_mark_minor
 from rai_compos_pub.opt_align_mark import Opt_align_mark
 
 from rai_compos_pub.logo import Logo
@@ -36,5 +29,7 @@ from rai_compos_pub.vialess_msl import VialessMSL
 from rai_compos_pub.wafer import Wafer 
 
 from rai_compos_pub.invert_layer import Invert_Layer
+
+from rai_compos_pub.ppc_kid import PPCKID
 
 from rai_compos_pub.ppckid_v2 import PPCKID_Assembly_v2
