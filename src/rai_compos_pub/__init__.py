1f896bd785a887bb3eed7702bf3d306059fdc5e3
Leon <138937349+Loslosser@users.noreply.github.com>
2025-04-17 13:59:33 +0200

    Update __init__.py

--- src/rai_compos_pub/__init__.py
+++ src/rai_compos_pub/__init__.py
@@ -35,3 +35,4 @@ from rai_compos_pub.ppc_kid import PPCKID
 from rai_compos_pub.ppckid_v2 import PPCKID_Assembly_v2
 
 from rai_compos_pub.dc_chip import DC_chip
+from rai_compos_pub.dc_chip import DC_chip_monolayer
