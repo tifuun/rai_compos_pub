887a67ff641d897fb250a62e55e9cfcc6eb523e8
Leon <138937349+Loslosser@users.noreply.github.com>
2025-07-23 10:49:35 +0200

    Update invert_layer.py
    Change Options to Option.Geometric

--- src/rai_compos_pub/invert_layer.py
+++ src/rai_compos_pub/invert_layer.py
@@ -190,23 +190,23 @@ class Invert_Layer(rai.Compo):
     """
 
     class Options:
-        outer_compo = rai.Option.Compos(
+        outer_compo = rai.Option.Geometric(
             "outer Compo that that encloses the inner compos",
             browser_default = rai.RectLW(10,10).proxy()
         )
-        inner_compo = rai.Option.Compos(
+        inner_compo = rai.Option.Geometric(
             "inner Compo to in enclosed by the outer compo",
             browser_default = rai.Circle(3).proxy()
         )
-        outer_layer = rai.Option.Layers(
+        outer_layer = rai.Option.Geometric(
             "specified layer-name of the outer compo",
             browser_default = "root"
         )
-        inner_layer= rai.Option.Layers(
+        inner_layer= rai.Option.Geometric(
             "specified layer-name of the inner compo",
             browser_default = "root"
         )
-        rev_inner = rai.Option.Compos(
+        rev_inner = rai.Option.Geometric(
             "reverses the order in which the inner compo contour is followed (clockwise or anti-clockwise)",
             browser_default = True
         )
