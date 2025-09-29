66b94215f0f2253c4311d5bf1ce47d111091fadb
Leon <138937349+Loslosser@users.noreply.github.com>
2025-09-29 15:22:43 +0200

    Update raitext.py

--- src/rai_compos_pub/raitext.py
+++ src/rai_compos_pub/raitext.py
@@ -241,7 +241,7 @@ class RAIText(rai.Compo):
                 translation += 20*scale
             elif string_chars[i] == "0":
                 self.subcompos[f"Object_{i}"] = symbols_dict[string_chars[i]].proxy().scale(scale).move(translation,0)
-                self.subcompos[f"Object_{i}_2"] = rai.CustomPoly([(20,45),(30,45),(30,25),(20,25)]).proxy().scale(scale).move((translation),0)
+                self.subcompos[f"Object_{i}_2"] = rai.CustomPoly([(20,25),(30,25),(30,45),(20,45)]).proxy().scale(scale).move((translation),0)
                 translation += 60*scale ##Fontwidht 60
             elif string_chars[i] == "|":
                 self.subcompos[f"Object_{i}"] = symbols_dict[string_chars[i]].proxy().scale(scale).move(translation,0)
